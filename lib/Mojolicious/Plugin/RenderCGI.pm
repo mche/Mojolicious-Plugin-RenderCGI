f22338133a27a4ac7d63067cbc5666a6fa9016c4
Mikhail Che <ernesto.chege@yandex.ru>
2016-05-18 16:31:49 +0500

    1

--- lib/Mojolicious/Plugin/RenderCGI.pm
+++ lib/Mojolicious/Plugin/RenderCGI.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::RenderCGI;
 
 use Mojo::Base 'Mojolicious::Plugin';
-
+use Capture::Tiny qw(capture);
 
 our $VERSION = '0.001';
 
@@ -11,21 +11,26 @@ sub register
   $app->renderer->add_handler(
     cgi => sub {
       my ($r, $c, $output, $options) = @_;
+      $options->{'format'} = 'cgi';
      
       # встроенный шаблон
       my $inline = $options->{inline};
      
-      # Сформировать относительный путь шаблона
+      # относительный путь шаблона
       my $name = $r->template_name($options);
      
-      # Попытка найти подходящий шаблон в секции DATA
+      # подходящий шаблон в секции DATA
       my $content = $r->get_data_template($options, $name);
      
-      # Сформировать абсолютный путь шаблона
+      #  абсолютный путь шаблона
       my $path = $r->template_path($options);
       
       #
-    
+      # capture from arbitrary code (Perl or external)
+ 
+      my ($stdout, $stderr, @result) = capture {
+        # your code here
+      };
     
      
       # Передать rendered результат обратно в рендерер
@@ -33,7 +38,9 @@ sub register
     }
   );
 }
- 
+
+
+
 1;
 
 
@@ -57,14 +64,48 @@ Mojolicious::Plugin::RenderCGI - Rendering template with Perl code CGI.pm subs e
 
 =head1 SYNOPSIS
 
-  use
+  $app->plugin('RenderCGI');
   
 
 =head1 Template
 
   my $foo = $c->stash('foo');
   $self->app->log->info("template rendering");
+  $c->include('far');
   say h1({}, $foo);
   say <<END_HTML;
-  <input name="bar" type="checkbox" />
+  <input name="bah" type="checkbox" />
   END_HTML
+  
+
+There are NO helpers without B<$c-\>> OR b<$self-\>> prefix.
+
+=head1 Options
+
+=head2 import
+
+  $app->plugin('RenderCGI', import=>':html -any');
+
+See at perldoc CGI.pm section "USING THE FUNCTION-ORIENTED INTERFACE". Default is ':html' (string) or [qw(:html)] (arrayref).
+
+
+=head1 SEE ALSO
+
+L<Mojolicious::Plugin::TagHelpers>
+
+=head1 AUTHOR
+
+Михаил Че (Mikhail Che), C<< <mche[-at-]cpan.org> >>
+
+=head1 BUGS / CONTRIBUTING
+
+Please report any bugs or feature requests at L<https://github.com/mche/Mojolicious-Plugin-RenderCGI/issues>. Pull requests also welcome.
+
+=head1 COPYRIGHT
+
+Copyright 2016 Mikhail Che.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
